cda6fa02d40804875bf5fbd2f17e7eaf2a22361e
David Salvatierra <air.salvatierra@gmail.com>
2018-08-02 12:36:32 -0400

    Vistas principales funcionando
    Se creo:
    - User Model
    - SignUp view
    - Form conectada al view
    - Urls.py de Account app

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls.urlsresolvers import revese_lazy
+from django.views.generic import CreateView
 
+from . import forms
 # Create your views here.
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = revese_lazy('login')
+    template_name = 'accounts/signup.html'
